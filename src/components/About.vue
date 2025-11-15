1845e51729971c046df3510e18f98e29f0ba0133
Shaun <lecoffeestain@gmail.com>
2025-11-15 21:46:33 +0000

    Revise About section with current job details
    Updated personal information and current roles in the About section.

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,9 +4,9 @@
             <div class="col-12 col-md-7">
                 <div class="display-6 border-bottom mb-3 pb-2">About me</div>
                 <div class="text-secondary">
-                    I'm a Ph.D student at <a href="https://www.shu.ac.uk/" target="shu">Sheffield Hallam University</a> with the <a href="https://serg.team" target="serg">Sports Engineering Research Group</a>, researching the use of machine learning to improve the
-                    performance of elite level athletes. I'm also a master of software engineering, working on a variety of projects. I've been a full stack developer for over 10 years, and have worked on and led teams in a variety of projects, from small websites to large scale
-                    e-commerce platforms and referencing software serving over 500k requests per day
+                    I'm a full time senior software enginner at <a href="https://seikosportstechnology.co.uk/" target="shu">Seiko Sports Technology</a> delivering precision timing at world athleteics events, as well as a Ph.D student at <a href="https://www.shu.ac.uk/" target="shu">Sheffield Hallam University</a> with the <a href="https://serg.team" target="serg">Sports Engineering Research Group</a>, researching the use of machine learning to improve the
+                    performance of elite level athletes. I'm also a master of software engineering, working on a variety of projects as self employed. I've been a full stack developer for over 10 years, and have worked on and led teams in a variety of projects, from small websites to large scale
+                    e-commerce platforms and referencing software serving over 500k requests per day. Full-time dad of 4 and student private pilot. Not wearing too many hats at all! <pre>Yes, thats me at bletchly park!</pre>
                 </div>
             </div>
             <div class="col mt-3 mt-md-0">
